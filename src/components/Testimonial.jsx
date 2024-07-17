79070d067b9fa996781f245cc0a55d031eeb8ad6
Your Name <you@example.com>
2024-07-17 23:37:37 +0530

    first commit

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -23,31 +23,3 @@ const Testimonial = () => {
 };
 
 export default Testimonial;
-
-
-// import React from "react";
-
-// const Testimonial = () => {
-//   return (
-//     <section
-//       className="section-padding bg-fixed bg-white overlay"
-//       style={{
-//         backgroundImage: `url(/img/bg-white.jpg)`,
-
-//         backgroundPosition: "0% -83.68px",
-//       }}
-//       data-stellar-background-ratio="0.8"
-//     >
-//       <div className="container">
-//         <div
-//           className="section-content aos-init aos-animate"
-//           data-aos="fade-up"
-//         >
-            
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default Testimonial;
