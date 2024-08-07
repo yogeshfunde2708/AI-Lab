66f7370f0948a47f124d84a32e572c49aa6072d3
Your Name <you@example.com>
2024-08-07 23:39:06 +0530

    navbar set-top

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,56 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "linearicons/dist/web-font/style.css";
 import SideNavbar from "./SideNavbar";
-import { useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import "../index.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 
 const Navbar = () => {
   const [showSideNavbar, setShowSideNavbar] = useState(false);
+  const [isTop, setIsTop] = useState(true);
   const sideNavbarRef = useRef(null);
+
+  useEffect(() => {
+    AOS.init({
+      easing: "ease",
+      duration: 400,
+      delay: 0,
+    });
+
+    const handleScroll = () => {
+      setIsTop(window.scrollY <= 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   const sideNavbar = () => {
     setShowSideNavbar(true);
   };
+
   const closeSideNavbar = () => {
     setShowSideNavbar(false);
   };
+
   return (
     <>
-      <div className="static-layout not-on-top">
-        <nav
-          id="header-navbar"
-          className="navbar navbar-expand-lg py-4 navbar-transparent"
-        >
+      <div
+        data-spy="scroll"
+        data-target="#navbar"
+        className={`static-layout ${isTop ? "navbar-transparent" : "not-on-top"}`}
+        data-aos-easing="ease"
+        data-aos-duration="400"
+        data-aos-delay="0"
+      >
+        <nav id="header-navbar" className="navbar navbar-expand-lg py-4">
           <div className="container">
-            <Link
-              className="navbar-brand d-flex align-items-center text-white"
-              to="/"
-            >
+            <Link className="navbar-brand d-flex align-items-center text-white" to="/">
               <h3 className="font-weight-bolder mb-0 border border-white p-2 m-2">
                 The AI Lab
               </h3>
@@ -35,7 +59,6 @@ const Navbar = () => {
               className="navbar-toggler"
               type="button"
               data-bs-toggle="collapse"
-              data-target="#navbar-nav-header"
               aria-expanded="false"
               aria-label="Toggle navigation"
               data-bs-target="#navbarSupportedContent"
@@ -43,10 +66,7 @@ const Navbar = () => {
             >
               <span className="lnr lnr-menu"></span>
             </button>
-            <div
-              className=" navbar-collapse collapse text-white"
-              id="navbarSupportedContent"
-            >
+            <div className="navbar-collapse collapse text-white" id="navbarSupportedContent">
               <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
                   <Link className="nav-link" to="/">
@@ -64,23 +84,18 @@ const Navbar = () => {
                   </Link>
                 </li>
                 <li className="nav-item only-desktop">
-                  <Link
-                    to=" "
-                    className="nav-link"
-                    id="side-nav-open"
-                    onClick={() => sideNavbar()}
-                  >
+                  <Link to="#" className="nav-link" id="side-nav-open" onClick={sideNavbar}>
                     <span className="lnr lnr-menu"></span>
                   </Link>
                 </li>
               </ul>
             </div>
           </div>
-          {showSideNavbar ? (
+          {showSideNavbar && (
             <div ref={sideNavbarRef}>
               <SideNavbar onClose={closeSideNavbar} />
             </div>
-          ) : null}
+          )}
         </nav>
       </div>
     </>
--- src/components/navComponents/Nav.jsx
+++ src/components/navComponents/Nav.jsx
@@ -1,16 +1,40 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "linearicons/dist/web-font/style.css";
 import "../../index.css";
 import SideNav from "./SideNav";
 import SideSearch from "./SideSearch";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Nav = () => {
   const [showSideNav, setShowSideNav] = useState(false);
   const [showSideSearch, setShowSideSearch] = useState(false);
   const sideNavRef = useRef(null);
   const sideSearchRef = useRef(null);
+  const [isTop, setIsTop] = useState(true);
 
+  useEffect(() => {
+    AOS.init({
+      easing: "ease",
+      duration: 400,
+      delay: 0,
+    });
+
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsTop(false);
+      } else {
+        setIsTop(true);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   const sideNav = () => {
     setShowSideNav(true);
   };
@@ -28,86 +52,87 @@ const Nav = () => {
   };
   return (
     <>
-      <nav
-        id="header-navbar"
-        className="navbar navbar-expand-lg py-4 navbar-transparent"
-      >
-        <div className="container">
-          <Link
-            className="navbar-brand d-flex align-items-center text-white"
-            to="/"
-          >
-            <h3 className="font-weight-bolder mb-0 border border-white p-2 m-2">
-              The AI Lab
-            </h3>
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-target="#navbar-nav-header"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-          >
-            <span className="lnr lnr-menu"></span>
-          </button>
-          <div
-            className="navbar-collapse collapse text-white"
-            id="navbarSupportedContent"
-          >
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/about">
-                  About
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/contact">
-                  Contact
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  to="#"
-                  className="nav-link"
-                  id="side-search-open"
-                  onClick={sideSearch}
-                >
-                  <span className="lnr lnr-magnifier"></span>
-                </Link>
-              </li>
-              <li className="nav-item only-desktop">
-                <Link
-                  to="#"
-                  className="nav-link"
-                  id="side-nav-open"
-                  onClick={sideNav}
-                >
-                  <span className="lnr lnr-menu"></span>
-                </Link>
-              </li>
-            </ul>
+      <div data-spy="scroll" data-target="#navabr" className={`static-layout ${isTop ? "navbar-transparent" : "not-on-top"}`} data-aos-easing="ease" data-aos-duration="400" data-aos-delay="0">
+        <nav
+          id="header-navbar"
+          className="navbar navbar-expand-lg py-4"
+        >
+          <div className="container">
+            <Link
+              className="navbar-brand d-flex align-items-center text-white"
+              to="/"
+            >
+              <h3 className="font-weight-bolder mb-0 border border-white p-2 m-2">
+                The AI Lab
+              </h3>
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+            >
+              <span className="lnr lnr-menu"></span>
+            </button>
+            <div
+              className="navbar-collapse collapse text-white"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/about">
+                    About
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/contact">
+                    Contact
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    to="#"
+                    className="nav-link"
+                    id="side-search-open"
+                    onClick={sideSearch}
+                  >
+                    <span className="lnr lnr-magnifier"></span>
+                  </Link>
+                </li>
+                <li className="nav-item only-desktop">
+                  <Link
+                    to="#"
+                    className="nav-link"
+                    id="side-nav-open"
+                    onClick={sideNav}
+                  >
+                    <span className="lnr lnr-menu"></span>
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-        {showSideNav && (
-          <div ref={sideNavRef}>
-            <SideNav onClose={closeSideNav} />
-          </div>
-        )}
+          {showSideNav && (
+            <div ref={sideNavRef}>
+              <SideNav onClose={closeSideNav} />
+            </div>
+          )}
 
-        {showSideSearch && (
-          <div ref={sideSearchRef}>
-            <SideSearch onClose={closeSideSearch} />
-          </div>
-        )}
-      </nav>
+          {showSideSearch && (
+            <div ref={sideSearchRef}>
+              <SideSearch onClose={closeSideSearch} />
+            </div>
+          )}
+        </nav>
+      </div>
     </>
   );
 };
